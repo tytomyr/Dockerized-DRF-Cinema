651f022dec2d0b8deada2a93684f9af5461136bb
Artur Tytomyr <art.tytomyr@gmail.com>
2023-05-01 22:01:59 +0300

    revised

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
